f0206e98956cc22bf636241893822c61d272cd44
Marc Bruce <braaco@gmail.com>
2017-04-24 21:34:18 +0200

    Chapter 1, Episode 3 Progress

--- Jakefile.js
+++ Jakefile.js
@@ -1,7 +1,37 @@
+/*global desc, task, jake, fail, complete */
+
+
 task("default", ["lint"]);
 
 desc("Lint everything");
 task("lint", [], function() {
     var lint = require("./build/lint/lint_runner.js");
-    lint.validateFile("Jakefile.js", {}, {});
+
+    var files = new jake.FileList();
+    files.include("**/*.js");
+    files.exclude("node_modules");
+    files.exclude("build");
+
+    var options = {
+        bitwise: true,
+        curly: false,
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        regexp: true,
+        undef: true,
+        strict: true,
+        trailing: true,
+        node: true
+    };
+
+    lint.validateFileList(files.toArray(), {}, {});
+
+
+
 });
